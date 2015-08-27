794b32e365c4a4f599654fab3386b2ee2980fc52
Robert Zhu <robzhu@gmail.com>
2015-08-27 15:54:53 -0400

    adding timestamp

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 var express = require('express');
+var format = require('date-format');
 var app = express();
 
 var motd = "no message specified, post one.";
+var lastUpdated = format.asString('hh:mm:ss.SSS', new Date());
 
 app.get('/', function(req,res){
-    res.send(motd);
+    res.send( lastUpdated + ': ' + motd);
 });
 
 app.post('/', function(req,res){
     var query = require('url').parse(req.url,true).query;
     motd = query.msg;
     console.log('motd set to: ' + motd);
+    lastUpdated = format.asString('hh:mm:ss.SSS', new Date());
     res.send(motd);
 });
 
