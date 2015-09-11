ec89c6038c0959a857a55f0ca649cefbb3385161
Robert Zhu <robzhu@gmail.com>
2015-09-11 12:01:12 -0400

    enabling cors

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var app = express();
 var motd = "no message specified, post one.";
 var lastUpdated = format.asString('hh:mm:ss.SSS', new Date());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req,res){
     res.send( lastUpdated + ': ' + motd);
 });
